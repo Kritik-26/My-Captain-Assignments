a0994e9797d783f0a018599f46c57187740b729e
Kritik-26 <117659331+Kritik-26@users.noreply.github.com>
2022-11-27 19:21:00 +0530

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+      
+r=1.1
+print("Input of the radius of circle r=1.1")
+pi=22/7
+a=pi*r**2
+print ("The area of the circle with radius 1.1 is ", a,"sq units" );
